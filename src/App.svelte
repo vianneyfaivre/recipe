ee6b7d210c0d3abf247de2ca01e7f79112e8281e
Vianney FAIVRE <vianney.x.faivre.-nd@disney.com>
2022-02-06 17:59:02 +0100

    error handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
 	import * as jsyaml from "js-yaml";
 	import { onMount } from 'svelte';
-	import type { PageDataDTO } from "./model/model";
+	import type { PageDataDTO } from "./model/yaml";
 	import Recipe from "./components/Recipe.svelte";
 
 	export let recipeYaml: string;
 
 	let pageData: PageDataDTO;
 	let selectedRecipeId: string;
+	let error;
 
 	async function loadYaml(path: string) {
 
 		const response = await fetch(path);
+
+		if(!response.ok) {
+			error = `Cause : HTTP ${response.status} - ${response.statusText} - ${path}`;
+			return;
+		}
+
 		const yaml = await response.text();
-
-		// TODO: basic error handling: fetch, parse yaml
-
 		const json = await jsyaml.load(yaml);
 
 		// Convert object to Map
@@ -26,11 +30,8 @@
 	
 	onMount(async () => {
 
-		// TODO: loading...
-
 		// Load yaml data
 		pageData = await loadYaml(recipeYaml); 
-		console.info(pageData)
 
 		// Select the first recipe by default
 		selectedRecipeId = pageData.recettes.keys().next().value;
@@ -44,47 +45,65 @@
 
 </script>
 
-{#if pageData && selectedRecipeId}
+<main>
 
-	<main>
+	<article class="post h-entry recette">
 
-		<article class="post h-entry recette">
+		{#if !!error}
 
 			<header>
-				<h1 class="post-title p-name">{pageData.title}</h1>
+				<h1 class="post-title p-name">Erreur !</h1>
 			</header>
-		
+
 			<div class="post-content e-content">
-		
-				<div class="variantes-tabs">
-				
-					{#each [...pageData.recettes] as [recipeId, recipe]}
-						<button 
-							class="variantes-btn" 
-							class:active={recipeId === selectedRecipeId}
-							on:click={e => onVarianteButtonClick(recipeId)}
-						>
-							{recipeId}
-						</button>
-					{/each}
-
-				</div>
-
-				{#each [...pageData.recettes] as [recipeId, recipe]}
-
-					<div class:hidden={recipeId !== selectedRecipeId}>
-						<Recipe {pageData} {recipeId} {recipe} />
-					</div>
-				
-				{/each}
-		
+				<p>Une erreur est survenue.</p>
+				<p>{error}</p>
 			</div>
+
+		{:else if pageData && selectedRecipeId}
+
+					<header>
+						<h1 class="post-title p-name">{pageData.title}</h1>
+					</header>
+				
+					<div class="post-content e-content">
+				
+						<div class="variantes-tabs">
+						
+							{#each [...pageData.recettes] as [recipeId, recipe]}
+								<button 
+									class="variantes-btn" 
+									class:active={recipeId === selectedRecipeId}
+									on:click={e => onVarianteButtonClick(recipeId)}
+								>
+									{recipeId}
+								</button>
+							{/each}
+
+						</div>
+
+						{#each [...pageData.recettes] as [recipeId, recipe]}
+
+							<div class:hidden={recipeId !== selectedRecipeId}>
+								<Recipe {pageData} {recipeId} {recipe} />
+							</div>
+						
+						{/each}
+				
+					</div>
+
+
+		{:else}
+
+			<header>
+				<h1 class="post-title p-name">Chargement en cours</h1>
+			</header>
+
+			<div class="post-content e-content">
+				<p>Chargement...</p>
+			</div>
+
+		{/if}
 		
-		</article>
-	</main>
-
-{:else}
-
-	<p>loading...</p>
-
-{/if}
+	</article>
+</main>
