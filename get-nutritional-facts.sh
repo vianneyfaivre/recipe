2fb75c420eea785a34d5aed2f4e540d5d1937195
Vianney FAIVRE <vianney.x.faivre.-nd@disney.com>
2022-03-05 18:27:07 +0100

    improved script

--- get-nutritional-facts.sh
+++ get-nutritional-facts.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-# Data Format https://world.openfoodfacts.org/data/data-fields.txt
+# Input params
+foodIds=("3451790012358" "3068110702235" "3564700370315" "20057251" "3174660000896" "3412290011074" "3068110361869" "3451790012358")
 
-wget https://world.openfoodfacts.org/api/v0/product/3451790012358.json -O extract.json
+for foodId in "${foodIds[@]}"
+do : 
+    # HTTP call
+    url="https://world.openfoodfacts.org/api/v0/product/$foodId.json"
+    destFile="nutrition-facts/$foodId.json"
 
-energy=$( cat extract.json | jq '.product | .nutriments | ."energy_value"' )
-unit=$( cat extract.json | jq '.product | .nutriments | ."energy_unit"' )
-carbs=$( cat extract.json | jq '.product | .nutriments | .carbohydrates_100g' )
-fat=$( cat extract.json | jq '.product | .nutriments | .fat_100g' )
-prot=$( cat extract.json | jq '.product | .nutriments | .proteins_100g' )
-salt=$( cat extract.json | jq '.product | .nutriments | .salt_100g' )
-fib=$( cat extract.json | jq '.product | .nutriments | .fiber_100g' )
+    wget $url --quiet --output-document=$destFile 
 
-echo $unit
+    # Get a human readable name
+    name=$( cat $destFile | jq --raw-output '.product | .categories_hierarchy[-1]' )
+    name=${name//:/_}
+    name=${name// /_}
+    name=${name//-/_}
 
-echo "const FOOD = new NutritionFactsDTO($energy, $carbs, $fat, $prot, $salt, ViscosityDTO.REPLACEME);"
+    # Rename dest file
+    mv $destFile "nutrition-facts/$name.json"
+
+    # Data Format https://world.openfoodfacts.org/data/data-fields.txt
+    energy=$( cat $destFile | jq '.product | .nutriments | ."energy_value"' )
+    unit=$( cat $destFile | jq '.product | .nutriments | ."energy_unit"' )
+    carbs=$( cat $destFile | jq '.product | .nutriments | .carbohydrates_100g' )
+    fat=$( cat $destFile | jq '.product | .nutriments | .fat_100g' )
+    prot=$( cat $destFile | jq '.product | .nutriments | .proteins_100g' )
+    salt=$( cat $destFile | jq '.product | .nutriments | .salt_100g' )
+    fib=$( cat $destFile | jq '.product | .nutriments | .fiber_100g' )
+
+
+    # Poor's man code generation
+    echo "const $name = new NutritionFactsDTO($energy, $carbs, $fat, $prot, $salt, ViscosityDTO.REPLACEME);"
+done
